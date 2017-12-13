2dd861045eaadfabfc8c0040be1459e297c37367
Rana <rana.bendary94@gmail.com>
2017-12-14 01:16:36 +0200

    App Skeleton

---
+++ Controllers/HostViewController.swift
@@ -0,0 +1,69 @@
+//
+//  HostViewController.swift
+//  Raye7AssignmentMapBox
+//
+//  Created by Rana El Bendary on 12/13/17.
+//  Copyright © 2017 Rana El Bendary. All rights reserved.
+//
+
+
+import UIKit
+import InteractiveSideMenu
+
+class HostViewController: MenuContainerViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let screenSize: CGRect = UIScreen.main.bounds
+        self.transitionOptions = TransitionOptions(duration: 0.4, visibleContentWidth: screenSize.width / 6)
+        
+        // Instantiate menu view controller by identifier
+        self.menuViewController = self.storyboard!.instantiateViewController(withIdentifier: "SideMenuListItemsViewController") as! MenuViewController
+        
+        // Gather content items controllers
+        self.contentViewControllers = contentControllers()
+        
+        // Select initial content controller. It's needed even if the first view controller should be selected.
+        self.selectContentViewController(contentViewControllers.first!)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        /*
+         Options to customize menu transition animation.
+         */
+        var options = TransitionOptions()
+        
+        // Animation duration
+        options.duration = size.width < size.height ? 0.4 : 0.6
+        
+        // Part of item content remaining visible on right when menu is shown
+        options.visibleContentWidth = size.width / 10
+        self.transitionOptions = options
+        
+    }
+    
+    
+    private func contentControllers() -> [UIViewController] {
+        let controllersIdentifiers = ["ViewController", "SavedLocationsViewController", "MapStylesViewController"]
+        var contentList = [UIViewController]()
+        
+        /*
+         Instantiate items controllers from storyboard.
+         */
+        for identifier in controllersIdentifiers {
+            if let viewController = self.storyboard?.instantiateViewController(withIdentifier: identifier) {
+                
+                contentList.append(viewController)
+            }
+        }
+        
+        return contentList
+    }
+    
+    
+}
+
+
---
+++ Controllers/SideMenuListItemsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SideMenuListItemsViewController.swift
+//  Raye7AssignmentMapBox
+//
+//  Created by Rana El Bendary on 12/13/17.
+//  Copyright © 2017 Rana El Bendary. All rights reserved.
+//
+
+import UIKit
+import InteractiveSideMenu
+
+class SideMenuListItemsViewController: MenuViewController , UITableViewDataSource, UITableViewDelegate{
+    
+    let kCellReuseIdentifier = "MenuCell"
+    let menuItems = ["Current Location", "My Saved Locations", "Map Styles"]
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCellReuseIdentifier, for: indexPath)
+        cell.textLabel?.text = menuItems[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let menuContainerViewController = self.menuContainerViewController else {
+            return
+        }
+        menuContainerViewController.selectContentViewController(menuContainerViewController.contentViewControllers[indexPath.row])
+        menuContainerViewController.hideSideMenu()
+    }
+    
+}
+
+
