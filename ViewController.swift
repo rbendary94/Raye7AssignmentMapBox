f044b826c16d81beb09844609eb1ef619084be94
Rana <rana.bendary94@gmail.com>
2017-12-14 00:53:14 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Raye7AssignmentMapBox
+//
+//  Created by Rana El Bendary on 12/14/17.
+//  Copyright © 2017 Rana El Bendary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
